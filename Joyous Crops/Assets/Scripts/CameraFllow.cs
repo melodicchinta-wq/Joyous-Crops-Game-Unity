07114cfdd67d0683daabbec72d531e8b27798fba
amelamelani35-source <amelamelani35@gmail.com>
2025-11-11 11:29:20 +0700

    scripts

---
+++ Joyous Crops/Assets/Scripts/CameraFllow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections;
+using UnityEngine;
+
+publis class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    Vector3 camOffse;
+
+    void Start()
+    {
+        camOffset = transform.position
+    }
+}
