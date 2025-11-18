3c9e58d9eb7c0ade49de7ce13dc0df2e0c808bad
melodicchinta-wq <melodicchinta@gmail.com>
2025-11-18 21:57:51 +0700

    Image

---
+++ Joyous Crops/Assets/Scripts/TalkInteract.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkInteract : Interactable
+{
+    public override void Interact(Character chracter)
+    {
+        Debug.Log("You talked with me congres");
+    }
+}
